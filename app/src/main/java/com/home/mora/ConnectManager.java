473ceffef2a7554b76e8f5dbc2be02838a32c994
yanchen184 <bobchen184@gmail.com>
2020-06-25 00:09:39 +0800

    CCC
    CCC

--- app/src/main/java/com/home/mora/ConnectManager.java
+++ app/src/main/java/com/home/mora/ConnectManager.java
@@ -22,9 +22,10 @@ public class ConnectManager extends AppCompatActivity {
 
     private Context context;
     private WebSocket webSocket;
-//    private GameActivity activity;
-
-    private static final String SERVER_PATH = "http://9262e4d681cb.ngrok.io";
+    private MainActivity activity;
+    private TextView txtCom;
+    private TextView txtWinLose;
+    private static final String SERVER_PATH = "http://6c6bc0455966.ngrok.io";
 
     /**
      * 用來避免改變tgBtn狀態時不知道是收到還是發送的狀況
@@ -36,7 +37,7 @@ public class ConnectManager extends AppCompatActivity {
 
     public ConnectManager(Context context) {
         this.context = context;
-//        activity = (GameActivity) context;
+        activity = (MainActivity) context;
     }
 
     public void initiateSocketConnection() {
@@ -95,77 +96,64 @@ public class ConnectManager extends AppCompatActivity {
      */
     private void receiveMessage(JSONObject jsonObject) {
 
-        View btnNo = findViewById(R.id.btnNo);
-        View btnYes = findViewById(R.id.btnYes);
-        View toNight = findViewById(R.id.toNight);
-
         isReceiving = true;
         try {
             switch (jsonObject.getString("kind")) {
-                case "createRoom":
-                    /**
-                     * 0.【kind】:createRoom
-                     * 1.獲得房間號碼放到create1234 【roomNum】:9453
-                     * **/
-                    /** 1.*/
-                    TextView create1234 = findViewById(R.id.create1234);
-                    create1234.setText(jsonObject.getString("roomNum"));
-
-                    isReceiving = false;
-                    break;
-                case "joinRoom":
-                    /**
-                     * 開房者 :
-                     * 0.【kind】:joinRoom
-                     * 1.獲得目前加入的人的名稱
-                     * 2.獲得目前人數
-                     * 進房者 :
-                     * 1.獲得是否成功加入房間
-                     * 2.獲得連上的人的名稱
-                     * **/
-                    TextView text1 = findViewById(R.id.textView1);
-                    TextView text2 = findViewById(R.id.textView2);
-
-                    if (jsonObject.getBoolean("joinBoolean")) {
-                        JSONObject j = new JSONObject(jsonObject.getString("otherPeople"));
-                        String roomerName = j.getString("roomerName");
-                        System.out.println("roomerName : " + roomerName);
-                        String roomNumber = j.getString("roomNumber");
-                        JSONArray ip = j.getJSONArray("ip");
-
-//                        roomerName = roomerName.substring(2,(roomerName.length()-2));
-
-                        EditText rn = findViewById(R.id.roomEditText);
-                        util.setAnnouncement(text1, text2, roomerName + "的房間(" + ip.length() + "人)", "連線房間號 : " + roomNumber);
-                        Toast.makeText(context, "連線房間號 : " + rn.getText(), Toast.LENGTH_SHORT).show();
-                        cancelConnect();
-                    } else {
-                        util.setAnnouncement(text1, text2, "查無此房間");
-                        Toast.makeText(context, "查無此房間", Toast.LENGTH_SHORT).show();
+                case "mora":
+                    int opponent = jsonObject.getInt("choose");
+                    System.out.println(opponent);
+                    txtCom = findViewById(R.id.txt_com);
+                    txtWinLose = findViewById(R.id.txt_winLose);
+                    if (activity.playerMora == 0) {
+                        switch (opponent) {
+                            case 0:
+                                txtCom.setText("電腦出拳頭");
+                                txtWinLose.setText("平手");
+                                break;
+                            case 1:
+                                txtCom.setText("電腦出剪刀");
+                                txtWinLose.setText("你贏了");
+                                break;
+                            case 2:
+                                txtCom.setText("電腦出布");
+                                txtWinLose.setText("你輸了");
+                                break;
+                        }
+                    } else if (activity.playerMora == 1) {
+                        switch (opponent) {
+                            case 0:
+                                txtCom.setText("電腦出拳頭");
+                                txtWinLose.setText("你輸了");
+                                break;
+                            case 1:
+                                txtCom.setText("電腦出剪刀");
+                                txtWinLose.setText("平手");
+                                break;
+                            case 2:
+                                txtCom.setText("電腦出布");
+                                txtWinLose.setText("你贏了");
+                                break;
+                        }
+                    } else if (activity.playerMora == 2) {
+                        switch (opponent) {
+                            case 0:
+                                txtCom.setText("電腦出拳頭");
+                                txtWinLose.setText("你贏了");
+                                break;
+                            case 1:
+                                txtCom.setText("電腦出剪刀");
+                                txtWinLose.setText("你輸了");
+                                break;
+                            case 2:
+                                txtCom.setText("電腦出布");
+                                txtWinLose.setText("平手");
+                                break;
+                        }
                     }
+
                     isReceiving = false;
                     break;
-                case "position":
-                    int pos = 0;
-                    boolean btnOn;
-                    pos = jsonObject.getInt("position");
-                    btnOn = jsonObject.getBoolean("btnOn");
-                    activity.order.get(pos - 1).setChecked(btnOn);
-                    isReceiving = false;
-                    break;
-                case "toNightClick":
-                    activity.onClickDayNight(toNight);
-                    isReceiving = false;
-                    break;
-                case "checkClick":
-                    boolean bool = jsonObject.getBoolean("choose");
-                    if (bool) {
-                        activity.onClickCheck(btnYes);
-                    } else {
-                        activity.onClickCheck(btnNo);
-                    }
-                    isReceiving = false;
-                    break;
+
             }
         } catch (JSONException e) {
             e.printStackTrace();
@@ -184,37 +172,9 @@ public class ConnectManager extends AppCompatActivity {
             JSONObject jsonObject = new JSONObject();
             try {
                 jsonObject.put("kind", kind);
-                String userId;
-                userId = activity.getSharedPreferences("name", MODE_PRIVATE)
-                        .getString("USER", "");
-                jsonObject.put("USER", userId);
                 switch (kind) {
-                    case "createRoom"://USER
-                        /**
-                         * 1.發送自己的名稱
-                         * **/
-                        /**使用者名稱 : if沒有使用者名稱 紀錄使用者名稱*/
-                        webSocket.send(jsonObject.toString());
-                        break;
-                    case "joinRoom"://USER & roomNum
-                        /**
-                         * 1.發送自己的名稱
-                         * 2.發送輸入的房間號碼
-                         * **/
-                        EditText roomEditText = findViewById(R.id.roomEditText);
-                        jsonObject.put("roomNum", roomEditText.getText().toString());
-                        webSocket.send(jsonObject.toString());
-                        break;
-                    case "position":
-                        jsonObject.put("position", num);
-                        jsonObject.put("btnOn", bool);
-                        webSocket.send(jsonObject.toString());
-                        break;
-                    case "toNightClick":
-                        webSocket.send(jsonObject.toString());
-                        break;
-                    case "checkClick":
-                        jsonObject.put("choose", bool);
+                    case "mora":
+                        jsonObject.put("choose", num);
                         webSocket.send(jsonObject.toString());
                         break;
                 }
@@ -229,10 +189,10 @@ public class ConnectManager extends AppCompatActivity {
      * bind with GameActivity
      */
     public void cancelConnect() {
-        View layout_pair = findViewById(R.id.layout_pair);
-        View layout_pair1 = findViewById(R.id.layout_pair1);
-        layout_pair.setVisibility(View.INVISIBLE);
-        layout_pair1.setVisibility(View.INVISIBLE);
+//        View layout_pair = findViewById(R.id.layout_pair);
+//        View layout_pair1 = findViewById(R.id.layout_pair1);
+//        layout_pair.setVisibility(View.INVISIBLE);
+//        layout_pair1.setVisibility(View.INVISIBLE);
     }
 
     @Override
--- app/src/main/java/com/home/mora/MainActivity.java
+++ app/src/main/java/com/home/mora/MainActivity.java
@@ -5,14 +5,20 @@ import androidx.lifecycle.ViewModelStoreOwner;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import okhttp3.WebSocket;
 
 public class MainActivity extends AppCompatActivity {
 
 
-    private TextView txtCom;
-    private int comMora;
-    private TextView txtWinLose;
+    private TextView txtCom, txtWinLose;
+    public int playerMora;
+    private EditText roomEditText;
+    WebSocket webSocket;
+    ConnectManager connectManager = new ConnectManager(this);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,60 +26,79 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         txtCom = findViewById(R.id.txt_com);
         txtWinLose = findViewById(R.id.txt_winLose);
+        connectManager.initiateSocketConnection();
+
+        roomEditText = findViewById(R.id.roomEditText);
+        roomEditText.setHint("----");
+    }
+
+    public void edit(View view) {
+
     }
 
     public void stone(View view) {
-        comMora = (int) (Math.floor(Math.random() * 3));
-        switch (comMora) {
-            case 0:
-                txtCom.setText("電腦出拳頭");
-                txtWinLose.setText("平手");
-                break;
-            case 1:
-                txtCom.setText("電腦出剪刀");
-                txtWinLose.setText("你贏了");
-                break;
-            case 2:
-                txtCom.setText("電腦出布");
-                txtWinLose.setText("你輸了");
-                break;
-        }
+//        comMora = (int) (Math.floor(Math.random() * 3));
+        connectManager.sendMessage(0, true, "mora");
+        playerMora = 0;
+        Toast.makeText(this, "出拳頭", Toast.LENGTH_SHORT).show();
+//        switch (comMora) {
+//            case 0:
+//                txtCom.setText("電腦出拳頭");
+//                txtWinLose.setText("平手");
+//                break;
+//            case 1:
+//                txtCom.setText("電腦出剪刀");
+//                txtWinLose.setText("你贏了");
+//                break;
+//            case 2:
+//                txtCom.setText("電腦出布");
+//                txtWinLose.setText("你輸了");
+//                break;
+//        }
 
     }
 
     public void scissors(View view) {
-        comMora = (int) (Math.floor(Math.random() * 3));
-        switch (comMora) {
-            case 0:
-                txtCom.setText("電腦出拳頭");
-                txtWinLose.setText("你輸了");
-                break;
-            case 1:
-                txtCom.setText("電腦出剪刀");
-                txtWinLose.setText("平手");
-                break;
-            case 2:
-                txtCom.setText("電腦出布");
-                txtWinLose.setText("你贏了");
-                break;
-        }
+//        comMora = (int) (Math.floor(Math.random() * 3));
+        connectManager.sendMessage(1, true, "mora");
+        playerMora = 1;
+        Toast.makeText(this, "出剪刀", Toast.LENGTH_SHORT).show();
+//        switch (comMora) {
+//            case 0:
+//                txtCom.setText("電腦出拳頭");
+//                txtWinLose.setText("你輸了");
+//                break;
+//            case 1:
+//                txtCom.setText("電腦出剪刀");
+//                txtWinLose.setText("平手");
+//                break;
+//            case 2:
+//                txtCom.setText("電腦出布");
+//                txtWinLose.setText("你贏了");
+//                break;
+//        }
     }
 
     public void cloth(View view) {
-        comMora = (int) (Math.floor(Math.random() * 3));
-        switch (comMora) {
-            case 0:
-                txtCom.setText("電腦出拳頭");
-                txtWinLose.setText("你贏了");
-                break;
-            case 1:
-                txtCom.setText("電腦出剪刀");
-                txtWinLose.setText("你輸了");
-                break;
-            case 2:
-                txtCom.setText("電腦出布");
-                txtWinLose.setText("平手");
-                break;
-        }
+//        comMora = (int) (Math.floor(Math.random() * 3));
+        connectManager.sendMessage(2, true, "mora");
+        Toast.makeText(this, "出布", Toast.LENGTH_SHORT).show();
+        playerMora = 2;
+//        switch (comMora) {
+//            case 0:
+//                txtCom.setText("電腦出拳頭");
+//                txtWinLose.setText("你贏了");
+//                break;
+//            case 1:
+//                txtCom.setText("電腦出剪刀");
+//                txtWinLose.setText("你輸了");
+//                break;
+//            case 2:
+//                txtCom.setText("電腦出布");
+//                txtWinLose.setText("平手");
+//                break;
+//        }
     }
+
+
 }
